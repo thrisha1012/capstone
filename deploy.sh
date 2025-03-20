ea49bb07653aa6c0b2c7aa46364bcb1e33f63980
THRISHA S <thrishamani2004@gmail.com>
2025-03-20 13:24:32 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u thrisha1012 -p dckr_pat__FpXRGOJ9p-qgqx0jraSOqitX7E
-    docker tag test thrisha1012/devops-image
+    docker tag test1 thrisha1012/devops-image
     docker push thrisha1012/devops-image
    
     
