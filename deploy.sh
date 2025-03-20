0a1559306e272c04f8fbce23b7adde8928a3521b
THRISHA S <thrishamani2004@gmail.com>
2025-03-20 14:02:59 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@
     docker login -u thrisha1012 -p dckr_pat__FpXRGOJ9p-qgqx0jraSOqitX7E
     docker tag test1 thrisha1012/devops-image
     docker push thrisha1012/devops-image
+    docker-compose up -d
    
     
